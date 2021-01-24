507552fe920f08b90fcbfb71a01a986d9d3c04c2
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-24 20:00:49 +0200

    Refractor getUserLocations.js

--- src/utils/getUserGeolocation.js
+++ src/utils/getUserGeolocation.js
@@ -1,19 +1,15 @@
-const onSuccess = (position) => {
-  const { latitude, longitude } = position.coords;
-  return { latitude, longitude };
-};
-
-const onError = (error) => ({ error: error.message });
-
 const getUserGeolocation = () =>
   new Promise((resolve, reject) => {
     if (!navigator.geolocation) {
       const errorMessage = new Error('Geolocation is not supported by this browser.');
-      reject(onError(errorMessage));
+      reject(errorMessage);
     } else {
       navigator.geolocation.getCurrentPosition(
-        (position) => resolve(onSuccess(position)),
-        (error) => reject(onError(error))
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          resolve({ latitude, longitude });
+        },
+        (error) => reject(error)
       );
     }
   });
