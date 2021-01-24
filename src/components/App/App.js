bc912e56d8cb920ea537585e56e5494549f93cd9
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-24 20:22:16 +0200

    Detect user's location on initial render

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './App.module.scss';
 import '../../iconLibrary';
+import { getCurrentLocation, selectLocation } from '../../actions';
 import SearchBar from '../SearchBar';
 import WeatherData from '../WeatherData';
 
-const App = () => (
-  <div className={styles.container}>
-    <SearchBar />
-    <WeatherData />
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+  const currentLocation = useSelector((state) => state.currentLocation.data);
+
+  // Get the current user location on initial render
+  useEffect(() => {
+    dispatch(getCurrentLocation());
+  }, []);
+
+  // Set the current location as selected if available (initialized to empty object)
+  useEffect(() => {
+    if (Object.keys(currentLocation).length > 0) {
+      dispatch(selectLocation(currentLocation));
+    }
+  }, [currentLocation]);
+
+  return (
+    <div className={styles.container}>
+      <SearchBar />
+      <WeatherData />
+    </div>
+  );
+};
 
 export default App;
