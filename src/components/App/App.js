aab11da72da44da5f8fd855c93c21a8412d3ecbb
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-25 12:30:19 +0200

    Move iconLibrary in utils folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './App.module.scss';
-import '../../iconLibrary';
+import '../../utils/iconLibrary';
 import {
   getCurrentLocation,
   selectLocation,
