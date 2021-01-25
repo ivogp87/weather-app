d213d375e51fb82b698033eb28195fbb1ce1d733
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-25 20:22:26 +0200

    Rename ForecastItem and make it more reusable

--- src/components/ForecastSummaryItem/ForecastSummaryItem.js
+++ src/components/ForecastSummaryItem/ForecastSummaryItem.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styles from './ForecastItem.module.scss';
+import styles from './ForecastSummaryItem.module.scss';
 import WeatherIcon from '../WeatherIcon';
+import Temperature from '../Temperature';
 import TemperatureRange from '../TemperatureRange';
 
-const ForecastItem = ({ date, weatherConditions, weatherIcon, minTemp, maxTemp, onClick }) => (
+const ForecastSummaryItem = ({
+  date,
+  weatherConditions,
+  weatherIcon,
+  temperature,
+  minTemp,
+  maxTemp,
+  onClick,
+}) => (
   <li
-    className={styles.forecastItem}
+    className={styles.summaryItem}
     role="presentation"
     title="Click to see more details"
     onClick={onClick}
@@ -20,19 +29,27 @@ const ForecastItem = ({ date, weatherConditions, weatherIcon, minTemp, maxTemp, 
       </span>
     </div>
     <div className={styles.itemsRight}>
-      <TemperatureRange min={minTemp} max={maxTemp} />
+      {temperature && <Temperature temp={temperature} />}
+      {minTemp && maxTemp && <TemperatureRange min={minTemp} max={maxTemp} />}
       <FontAwesomeIcon className={styles.icon} icon="chevron-down" size="lg" />
     </div>
   </li>
 );
 
-ForecastItem.propTypes = {
+ForecastSummaryItem.propTypes = {
   date: PropTypes.string.isRequired,
   weatherConditions: PropTypes.string.isRequired,
   weatherIcon: PropTypes.string.isRequired,
-  minTemp: PropTypes.number.isRequired,
-  maxTemp: PropTypes.number.isRequired,
+  temperature: PropTypes.number,
+  minTemp: PropTypes.number,
+  maxTemp: PropTypes.number,
   onClick: PropTypes.func.isRequired,
 };
 
-export default ForecastItem;
+ForecastSummaryItem.defaultProps = {
+  temperature: null,
+  minTemp: null,
+  maxTemp: null,
+};
+
+export default ForecastSummaryItem;
