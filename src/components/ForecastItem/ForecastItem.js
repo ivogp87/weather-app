3551a6d05a0266ed6bda7f0e1bbdf1c6a7f0a9df
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-25 16:58:16 +0200

    Add ForecastItem component

---
+++ src/components/ForecastItem/ForecastItem.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './ForecastItem.module.scss';
+import WeatherIcon from '../WeatherIcon';
+import TemperatureRange from '../TemperatureRange';
+
+const ForecastItem = ({ date, weatherConditions, weatherIcon, minTemp, maxTemp, onClick }) => (
+  <li
+    className={styles.forecastItem}
+    role="presentation"
+    title="Click to see more details"
+    onClick={onClick}
+  >
+    <div className={styles.primary}>
+      <WeatherIcon icon={weatherIcon} size="medium" />
+      <span>
+        <p>{date}</p>
+        <p>{weatherConditions}</p>
+      </span>
+    </div>
+    <div className={styles.secondary}>
+      <TemperatureRange min={minTemp} max={maxTemp} />
+      <FontAwesomeIcon className={styles.icon} icon="chevron-down" size="lg" />
+    </div>
+  </li>
+);
+
+ForecastItem.propTypes = {
+  date: PropTypes.string.isRequired,
+  weatherConditions: PropTypes.string.isRequired,
+  weatherIcon: PropTypes.string.isRequired,
+  minTemp: PropTypes.number.isRequired,
+  maxTemp: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired, // not req ?
+};
+
+export default ForecastItem;
