818eda404fd15cb25ba00c26c84a31bcb9156036
ivogp87 <ivaylopantaleev@hotmail.com>
2021-01-30 16:27:51 +0200

    Add props to IconButton

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './IconButton.module.scss';
 
-const IconButton = ({ type, icon, color, size, className, onClick }) => {
+const IconButton = ({ type, icon, color, size, className, title, onClick }) => {
   const classNames = `${styles.iconButton} ${styles[color]} ${styles[size]}${
     className && ` ${className}`
   }`;
@@ -13,6 +13,7 @@ const IconButton = ({ type, icon, color, size, className, onClick }) => {
       // eslint-disable-next-line react/button-has-type
       type={type}
       className={classNames}
+      title={title}
       onClick={onClick}
     >
       <FontAwesomeIcon icon={icon} />
@@ -26,6 +27,7 @@ IconButton.propTypes = {
   color: PropTypes.oneOf(['primary', 'secondary']),
   size: PropTypes.oneOf(['small', 'medium', 'large']),
   className: PropTypes.string,
+  title: PropTypes.string,
   onClick: PropTypes.func,
 };
 
@@ -33,6 +35,7 @@ IconButton.defaultProps = {
   color: 'primary',
   size: 'medium',
   className: '',
+  title: '',
   onClick: () => null,
 };
 
