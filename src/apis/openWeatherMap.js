83e09131882c771a9de188a93fa490cae5b0272c
ivogp87 <ivaylopantaleev@hotmail.com>
2021-02-10 15:18:54 +0200

    Change API url -  https

--- src/apis/openWeatherMap.js
+++ src/apis/openWeatherMap.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
 
 const openWeatherMap = axios.create({
-  baseURL: 'http://api.openweathermap.org',
+  baseURL: 'https://api.openweathermap.org',
   params: {
     appid: API_KEY,
   },
