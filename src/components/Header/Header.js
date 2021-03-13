fbb1b1d58ccd019c3353a76bf98c31f51a6ab638
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-13 21:04:07 +0200

    Add new Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.scss';
+
+const Header = () => (
+  <header className={styles.header}>
+    <h1>What Is The Weather In</h1>
+  </header>
+);
+
+export default Header;
