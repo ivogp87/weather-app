08fda7ed29abd3a41c450fc500cc4134d7c6b166
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-13 21:28:04 +0200

    Add axios interceptors

--- src/apis/openWeatherMap.js
+++ src/apis/openWeatherMap.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+import removeDuplicateLocations from '../utils/removeDuplicateLocations';
+
 const API_KEY = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
 
 const openWeatherMap = axios.create({
@@ -8,5 +10,24 @@ const openWeatherMap = axios.create({
     appid: API_KEY,
   },
 });
+
+openWeatherMap.interceptors.response.use((response) => {
+  if (response.config.url.includes('/geo/')) {
+    const uniqueLocations = removeDuplicateLocations(response.data);
+    return { ...response, data: uniqueLocations };
+  }
+  return response;
+});
+
+export const getLocationName = (latitude, longitude) =>
+  openWeatherMap.get(`/geo/1.0/reverse?lat=${latitude}&lon=${longitude}`);
+
+export const getLocationsByName = (locationName) =>
+  openWeatherMap.get(`/geo/1.0/direct?q=${locationName}&limit=10`);
+
+export const getWeatherForecast = (latitude, longitude) =>
+  openWeatherMap.get(
+    `/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=minutely`
+  );
 
 export default openWeatherMap;
