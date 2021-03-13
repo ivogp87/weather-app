563c2ce48e83ef77ee310c0b8d56e662fb7b3756
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-13 21:23:15 +0200

    Remove old component from App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,63 +1,18 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React from 'react';
 import styles from './App.module.scss';
 import '../../utils/iconLibrary';
-import {
-  getCurrentLocation,
-  selectLocation,
-  fetchWeatherForecast,
-  restoreSavedLocations,
-} from '../../actions';
-import SearchBar from '../SearchBar';
-import FavoriteLocations from '../FavoriteLocations';
-import CurrentWeatherContainer from '../CurrentWeatherContainer';
-import WeeklyForecast from '../WeeklyForecast';
+import Header from '../Header';
+import SearchContainer from '../SearchContainer';
+import FavoriteLocationsContainer from '../FavoriteLocationsContainer';
+import WeatherContainer from '../WeatherContainer';
 
-const App = () => {
-  const dispatch = useDispatch();
-  const { latitude, longitude } = useSelector((state) => state.selectedLocation);
-  const currentLocation = useSelector((state) => state.currentLocation.data);
-  const favoriteLocations = useSelector((state) => state.favoriteLocations);
-
-  useEffect(() => {
-    dispatch(getCurrentLocation());
-
-    const savedLocations = localStorage.getItem('favoriteLocations');
-    if (savedLocations) {
-      const locationsArray = JSON.parse(savedLocations);
-      dispatch(restoreSavedLocations(locationsArray));
-    }
-  }, [dispatch]);
-
-  useEffect(() => {
-    if (Object.keys(currentLocation).length > 0) {
-      dispatch(selectLocation(currentLocation));
-    }
-  }, [currentLocation, dispatch]);
-
-  useEffect(() => {
-    if (latitude && longitude) {
-      dispatch(fetchWeatherForecast(latitude, longitude));
-    }
-  }, [latitude, longitude, dispatch]);
-
-  useEffect(() => {
-    if (favoriteLocations.length > 0) {
-      const locations = JSON.stringify(favoriteLocations);
-      localStorage.setItem('favoriteLocations', locations);
-    } else {
-      localStorage.removeItem('favoriteLocations');
-    }
-  }, [favoriteLocations]);
-
-  return (
-    <div className={styles.container}>
-      <SearchBar />
-      <FavoriteLocations />
-      <CurrentWeatherContainer />
-      <WeeklyForecast />
-    </div>
-  );
-};
+const App = () => (
+  <div className={styles.container}>
+    <Header />
+    <SearchContainer />
+    <FavoriteLocationsContainer />
+    <WeatherContainer />
+  </div>
+);
 
 export default App;
