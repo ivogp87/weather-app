2e586bd7f7d49eb9723e3732e0b7608a95037d88
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-13 21:43:46 +0200

    Delee old actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,5 @@
-export { default as selectLocation } from './selectLocation';
 export { default as fetchLocationsList } from './fetchLocationsList';
 export { default as fetchWeatherForecast } from './fetchWeatherForecast';
-export { default as saveLocation } from './saveLocation';
-export { default as removeLocation } from './removeLocation';
 export { default as restoreSavedLocations } from './restoreSavedLocations';
-export { default as getCurrentLocation } from './getCurrentLocation';
+export { default as toggleFavoriteLocations } from './toggleFavoriteLocations';
+export { default as fetchLocationDetails } from './fetchLocationDetails';
--- src/actions/selectLocation.js
+++
@@ -1,8 +0,0 @@
-import { SELECT_LOCATION } from './actionTypes';
-
-const selectLocation = (locationObject) => ({
-  type: SELECT_LOCATION,
-  payload: locationObject,
-});
-
-export default selectLocation;
