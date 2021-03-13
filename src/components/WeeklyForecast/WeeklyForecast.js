1c82c52d8c3c1ed61d260f42476d92125aeda27f
ivogp87 <ivaylopantaleev@hotmail.com>
2021-03-13 21:19:50 +0200

    Improve WeeklyForecast

--- src/components/WeeklyForecast/WeeklyForecast.js
+++ src/components/WeeklyForecast/WeeklyForecast.js
@@ -1,39 +1,34 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 import Card from '../Card';
 import DailyForecast from '../DailyForecast';
 
-const WeeklyForecast = () => {
-  const weeklyForecast = useSelector((state) => state.weatherForecast.data.daily);
+const WeeklyForecast = ({ forecastData }) => (
+  <Card>
+    <h2>Weekly Forecast</h2>
+    {forecastData.map(({ dayOfWeek, weatherDescription, weatherIcon, minTemp, maxTemp }) => (
+      <DailyForecast
+        key={dayOfWeek}
+        dayOfWeek={dayOfWeek}
+        weatherDescription={weatherDescription}
+        weatherIcon={weatherIcon}
+        minTemp={minTemp}
+        maxTemp={maxTemp}
+      />
+    ))}
+  </Card>
+);
 
-  if (!weeklyForecast) return null;
-
-  return (
-    <Card>
-      <h2>Weekly Forecast</h2>
-      {weeklyForecast.map((day) => {
-        const { dt: unixTimestamp, weather, temp } = day;
-        const { description, icon } = weather[0];
-        const date = new Date(unixTimestamp * 1000);
-        const dateToday = new Date(Date.now()).getDate();
-        const dayOfWeek =
-          dateToday === date.getDate()
-            ? 'Today'
-            : date.toLocaleDateString('en-US', { weekday: 'long' });
-
-        return (
-          <DailyForecast
-            key={unixTimestamp}
-            dayOfWeek={dayOfWeek}
-            weatherDescription={description}
-            weatherIcon={icon}
-            minTemp={temp.min}
-            maxTemp={temp.max}
-          />
-        );
-      })}
-    </Card>
-  );
+WeeklyForecast.propTypes = {
+  forecastData: PropTypes.arrayOf(
+    PropTypes.shape({
+      dayOfWeek: PropTypes.string.isRequired,
+      maxTemp: PropTypes.number.isRequired,
+      minTemp: PropTypes.number.isRequired,
+      weatherDescription: PropTypes.string.isRequired,
+      weatherIcon: PropTypes.string.isRequired,
+    })
+  ).isRequired,
 };
 
 export default WeeklyForecast;
